630b694f2d9210e15d6c8aca8bc944de3fa857ee
freakyshine <p.daniel@medalu.ch>
2021-06-24 15:56:13 +0200

    Adding Project Solution

---
+++ Regex_M226a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Regex_M226a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
