80d37d10bd1887b3550a4b3ddab2cb8c10f9e347
Daniel Lutz <daniel.lutz@mobilezoneholding.net>
2021-06-29 13:56:20 +0200

    Adding exitCode parameter to the AudibleExit method and adding additional XML comments

--- Regex_M226a/Program.cs
+++ Regex_M226a/Program.cs
@@ -12,16 +12,26 @@ namespace Regex_M226a
             Console.WriteLine("**     What would you like to do?     **\n" +
                               "** * * * * * * * * * * * * * * * * *  **");
 
-            AudibleExit();
+            AudibleExit(0);
         }
+        /// <summary>
+        /// The User Menu is used to display the menu and also contains the logic for the menu since it is not that complicated
+        /// </summary>
         static void UserMenu()
         {
 
         }
-        static void AudibleExit ()
+        /// <summary>
+        /// The audible exit method keeps the console from just closing like that. It tells the user the program has ended and prompt the user to enter a key. Then the program will be exited by the method
+        /// </summary>
+        /// <param name="exitCode">
+        /// The exit code with which the program will be exited
+        /// </param>
+        static void AudibleExit (int exitCode)
         {
             Console.WriteLine("\nPress any key to exit the program.");
             Console.ReadKey();
+            Environment.Exit(exitCode);
         }
     }
 }
