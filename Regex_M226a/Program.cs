5d98f27f496b9ad7eddf4394c33f62f00d215ba7
Daniel Lutz <daniel.lutz@mobilezoneholding.net>
2021-06-29 14:20:40 +0200

    Adding menu logic and output, adding 1.5s timeout before showing menu

--- Regex_M226a/Program.cs
+++ Regex_M226a/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Regex_M226a
 {
@@ -9,8 +10,10 @@ namespace Regex_M226a
             Console.WriteLine("** ********************************** **\n" +
                               "** Welcome to the RegEx code example! **\n" +
                               "** ********************************** **");
-            Console.WriteLine("**     What would you like to do?     **\n" +
-                              "** * * * * * * * * * * * * * * * * *  **");
+            Thread.Sleep(1500); // Keep Thread idle for 1.5s
+
+            // Call menu
+            UserMenu();
 
             AudibleExit(0);
         }
@@ -19,7 +22,34 @@ namespace Regex_M226a
         /// </summary>
         static void UserMenu()
         {
-
+            ConsoleKey userInput;
+            byte menuPointer = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("** * * * * * * * * * * * * * * * * *  **\n" +
+                                  "**     What would you like to do?     **\n" +
+                                  "** * * * * * * * * * * * * * * * * *  **");
+                Console.Write($"[{(menuPointer == 0 ? '*': ' ')}] Option 1\n" +
+                              $"[{(menuPointer == 1 ? '*' : ' ')}] Option 2\n" +
+                              $"[{(menuPointer == 2 ? '*' : ' ')}] Option 3");
+                userInput = Console.ReadKey().Key;
+                switch (userInput)
+                {
+                    case ConsoleKey.UpArrow:
+                        menuPointer--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        menuPointer++;
+                        break;
+                    default:
+                        break;
+                }
+                menuPointer = (byte)(menuPointer > 3 ? 2 : menuPointer);
+                menuPointer %= 3;
+            } while (userInput != ConsoleKey.Enter);
+            Console.WriteLine("[]");
+            Console.Clear();
         }
         /// <summary>
         /// The audible exit method keeps the console from just closing like that. It tells the user the program has ended and prompt the user to enter a key. Then the program will be exited by the method
