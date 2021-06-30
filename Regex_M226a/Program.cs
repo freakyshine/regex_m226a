47de3d1240c26f1abc894b943e76ae4e9e12905c
freakyshine <daniel@lutz.is>
2021-06-30 20:37:08 +0200

    Adding password RegEx example method, changing return value type to byte, removing unnecessary Console.WriteLine after menu,

--- Regex_M226a/Program.cs
+++ Regex_M226a/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Regex_M226a
 {
     class Program
     {
+        private static Regex passwordRegEx = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W])[A-Za-z\d\W]{8,}$");
+        private static Regex digits = new Regex(@"[\d]{1,}");
+        private static Regex romanLetters = new Regex(@"[A-Za-z]{1,}");
+        private static Regex symbols = new Regex(@"[\W]{1,}");
+
         static void Main(string[] args)
         {
             Console.WriteLine("** ********************************** **\n" +
@@ -12,15 +18,28 @@ namespace Regex_M226a
                               "** ********************************** **");
             Thread.Sleep(1500); // Keep Thread idle for 1.5s
 
-            // Call menu
-            UserMenu();
+			// Todo: Implement loading screen
+
+			// Call menu
+			switch (UserMenu())
+			{
+                case 0:
+                    PasswordExample();
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+					break;
+			}
 
             AudibleExit(0);
         }
         /// <summary>
         /// The User Menu is used to display the menu and also contains the logic for the menu since it is not that complicated
         /// </summary>
-        static void UserMenu()
+        static byte UserMenu()
         {
             ConsoleKey userInput;
             byte menuPointer = 0;
@@ -48,8 +67,8 @@ namespace Regex_M226a
                 menuPointer = (byte)(menuPointer > 3 ? 2 : menuPointer);
                 menuPointer %= 3;
             } while (userInput != ConsoleKey.Enter);
-            Console.WriteLine("[]");
             Console.Clear();
+            return menuPointer;
         }
         /// <summary>
         /// The audible exit method keeps the console from just closing like that. It tells the user the program has ended and prompt the user to enter a key. Then the program will be exited by the method
@@ -62,6 +81,36 @@ namespace Regex_M226a
             Console.WriteLine("\nPress any key to exit the program.");
             Console.ReadKey();
             Environment.Exit(exitCode);
+        }
+        /// <summary>
+        /// Output the example for password regex
+        /// </summary>
+        static void PasswordExample()
+        {
+            Console.WriteLine("** ********************************** **\n" +
+                              "**    Example RegEx for passwords     **\n" +
+                              "** ********************************** **");
+            Console.WriteLine("Enter a password:");
+            string newPassword = Console.ReadLine();
+            
+			while (!passwordRegEx.IsMatch(newPassword))
+			{
+                Console.Clear();
+                Console.WriteLine("Password is not secure enough");
+                Console.WriteLine("Criteria:\n" +
+                                  $"[{(!romanLetters.IsMatch(newPassword) ? ' ' : 'X')}] Contain at least 1 letter\n" +
+                                  $"[{(!digits.IsMatch(newPassword) ? ' ' : 'X')}] Contain at least 1 digit\n" +
+                                  $"[{(!symbols.IsMatch(newPassword) ? ' ' : 'X')}] Contain at least 1 special character");
+                
+                Console.WriteLine("Enter a password:");
+                newPassword = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.Write("Your password is set to: ");
+			for (int i = 0; i < newPassword.Length; i++)
+			{
+                Console.Write("*");
+			}
         }
     }
 }
